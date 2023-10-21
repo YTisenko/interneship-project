becbb9a69bcf8215bb533b6a79edc3d1c07c37fc
Yulia <tissiol@yahoo.com>
2023-10-20 21:16:28 -0400

    added secondary deals filtering

---
+++ features/steps/secondary_option.py
@@ -0,0 +1,70 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+SEARCH_INPUT = (By.NAME,)
+SEARCH_SUBMIT = (By.NAME,)
+
+
+@given("Open the main page")
+def step_open_main_page(context):
+    context.driver.get("https://soft.reelly.io/sign-in")
+
+@when("User logs in")
+def step_user_logs_in(context):
+    email = "first@email.com"
+    password = "careerist"
+    email_field = context.driver.find_element(By.ID, "email-2")
+    password_field = context.driver.find_element(By.ID, "field")
+    continue_button = context.driver.find_element(By.CLASS_NAME, "login-button w-button")
+    email_field.send_keys(email)
+    password_field.send_keys(password)
+    continue_button.click()
+    sleep(2)
+
+@when("User clicks on the Secondary option in the left side menu")
+def step_user_clicks_secondary_option(context):
+    secondary_menu_option = context.driver.find_element(By.XPATH,"//div[contains(text(),'Secondary')]")
+    secondary_menu_option.click()
+    sleep(2)
+
+@then("Correct page is open")
+def step_verify_correct_page(context):
+    expected_url= "https://soft.reelly.io/secondary-listings"
+    actual_url = context.driver.current_url
+    assert expected_url == actual_url, f"Expected URL: {expected_url}, Actual URL: {actual_url}"
+
+@when("User filters the products by 'want to buy'")
+def step_filter_products_by_want_to_buy(context):
+    filter_element = context.driver.find_element(By.CSS_SELECTOR, ".filter-button")
+    filter_element.click()
+    sleep(2)
+    want_to_buy_element= context.driver.find_element(By.XPATH, "//div[@class='switcher-button']//div[contains(text(), 'Want to buy')]")
+    want_to_buy_element.click()
+    sleep(2)
+    apply_filter_element=context.driver.find_element (By.XPATH, "//a[contains(text(), 'Apply filter') and contains(@class, 'button-filter')]")
+    apply_filter_element.click()
+    sleep(2)
+    expected_url="https://soft.reelly.io/secondary-listings"
+    actual_url=context.driver.current_url
+    assert expected_url == actual_url, f"Expected URL: {expected_url}, Actual URL: {actual_url}"
+
+@then("Verify all cards have a 'want to buy' tag")
+def step_verify_cards_have_want_to_buy(context):
+    expected_tag=context.driver.find_element(By.XPATH, "//div[@wized='saleTagMLS' and contains(text(), 'want to buy')]")
+    all_cards=context.driver.find_element(By.XPATH,"//div[@wized='listingCardMLS']")
+    all_cards_have_tag=True
+    for card in cards:
+        if expected_tag not in all_cards:
+            all_cards_have_tag=False
+            break
+    assert all_cards_have_tag, "All cards have the 'want to buy' tag."
+
+
+
+
+
+
+
+
+
